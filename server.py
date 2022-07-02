a0a8effad9601da926582f573e2004494efe44e7
Anmol Budhiraja <34093007+anmoldtu@users.noreply.github.com>
2022-07-02 16:07:11 +0530

    Add Function Call to set Default Arguments

--- server.py
+++ server.py
@@ -72,7 +72,7 @@ def run_machine_learning(clients, args, poisoned_workers):
 
     return convert_results_to_csv(epoch_test_set_results), worker_selection
 
-def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_strategy, idx):
+def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_strategy, idx, net):
     log_files, results_files, models_folders, worker_selections_files = generate_experiment_ids(idx, 1)
 
     # Initialize logger
@@ -81,6 +81,7 @@ def run_exp(replacement_method, num_poisoned_workers, KWARGS, client_selection_s
     args = Arguments(logger)
     args.set_model_save_path(models_folders[0])
     args.set_num_poisoned_workers(num_poisoned_workers)
+    args.set_default_args(net)
     args.set_round_worker_selection_strategy_kwargs(KWARGS)
     args.set_client_selection_strategy(client_selection_strategy)
     args.log()
