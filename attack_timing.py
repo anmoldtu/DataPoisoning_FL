3a9175ffe2fd3367f7fa84c694870ee291fed375
Anmol Budhiraja <34093007+anmoldtu@users.noreply.github.com>
2022-07-02 16:35:44 +0530

    Provide Command Line Arguments Capability

--- attack_timing.py
+++ attack_timing.py
@@ -7,19 +7,49 @@ from federated_learning.utils import replace_6_with_0
 from federated_learning.worker_selection import BeforeBreakpoint
 from federated_learning.worker_selection import AfterBreakpoint
 from server import run_exp
+import argparse
 
 if __name__ == '__main__':
-    START_EXP_IDX = 3000
-    NUM_EXP = 3
-    NET = "cifar"
-    NUM_POISONED_WORKERS = 5
-    REPLACEMENT_METHOD = replace_1_with_9
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--NUM_EXP')
+    parser.add_argument('--START_EXP_IDX')
+    parser.add_argument('--NUM_POISONED_WORKERS')
+    parser.add_argument('--NET')
+    parser.add_argument('--REPLACEMENT_METHOD')
+    parser.add_argument('--BREAKPOINT')
+    args = parser.parse_args()
+    
+    
+    START_EXP_IDX = int(args.START_EXP_IDX)
+    NUM_EXP = int(args.NUM_EXP)
+    NET = args.NET
+    NUM_POISONED_WORKERS = int(args.NUM_POISONED_WORKERS)
+    
+    REPLACEMENT_METHOD = args.REPLACEMENT_METHOD
+    if(REPLACEMENT_METHOD == 'replace_0_with_2'):
+        REPLACEMENT_METHOD = replace_0_with_2
+    elif(REPLACEMENT_METHOD == 'replace_5_with_3'):
+        REPLACEMENT_METHOD = replace_5_with_3
+    elif(REPLACEMENT_METHOD == 'replace_1_with_9'):
+        REPLACEMENT_METHOD = replace_1_with_9
+    elif(REPLACEMENT_METHOD == 'replace_4_with_6'):
+        REPLACEMENT_METHOD = replace_4_with_6
+    elif(REPLACEMENT_METHOD == 'replace_1_with_3'):
+        REPLACEMENT_METHOD = replace_1_with_3
+    elif(REPLACEMENT_METHOD == 'replace_6_with_0'):
+        REPLACEMENT_METHOD = replace_6_with_0
     KWARGS = {
         "BeforeBreakPoint_EPOCH" : 75,
-        "BeforeBreakpoint_NUM_WORKERS_PER_ROUND" : 5,
+        "BeforeBreakpoint_NUM_WORKERS_PER_ROUND" : 10,
         "AfterBreakPoint_EPOCH" : 75,
-        "AfterBreakpoint_NUM_WORKERS_PER_ROUND" : 5,
+        "AfterBreakpoint_NUM_WORKERS_PER_ROUND" : 10,
     }
-
+    
+    BreakPoint = AfterBreakpoint()
+    
+    if(args.BREAKPOINT == 'before'):
+        BreakPoint = BeforeBreakpoint()
+        
     for experiment_id in range(START_EXP_IDX, START_EXP_IDX + NUM_EXP):
-        run_exp(REPLACEMENT_METHOD, NUM_POISONED_WORKERS, KWARGS, AfterBreakpoint(), experiment_id, NET)
+        run_exp(REPLACEMENT_METHOD, NUM_POISONED_WORKERS, KWARGS, BreakPoint, experiment_id, NET)
